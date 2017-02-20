ab67bfe49219b806b9130ceb207745822b79ddd9
andy <andymaheshw@gmail.com>
2017-02-20 08:57:21 -0500

    removing sass

--- app.js
+++ app.js
@@ -64,10 +64,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(expressStatusMonitor());
 app.use(compression());
-app.use(sass({
+/*app.use(sass({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public')
-}));
+}));*/
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
